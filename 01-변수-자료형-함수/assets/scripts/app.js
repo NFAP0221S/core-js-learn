e0adf87e5013b3f5f1f1af189f5c9c854669c2fa
Hansol <102638663+NFAP0221S@users.noreply.github.com>
2023-09-19 23:43:06 +0900

    21. 변수 & 연산자로 작업하기

--- 01-변수-자료형-함수/assets/scripts/app.js
+++ 01-변수-자료형-함수/assets/scripts/app.js
@@ -1,1 +1,9 @@
-alert('This works!');
+// 변수 및 연산자 작업
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+currentResult = (currentResult + 10) * 3 / 2 - 1;
+
+let calculationDescription = '(' + defaultResult + ' + 10) * 3 / 2 - 1';
+
+outputResult(currentResult, calculationDescription);
