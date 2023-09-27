3ea42e71185ded879abab3e7a582f276199ed4d7
Hansol <102638663+NFAP0221S@users.noreply.github.com>
2023-09-27 23:56:59 +0900

    feat: 유저, 몬스터 attack 기능

---
+++ 03-몬스터킬[제어문]/assets/scripts/app.js
@@ -0,0 +1,30 @@
+// 유저 공격 값
+const ATTAK_VALUE = 10;
+// 몬스터 공격 값
+const MONSTER_ATTAK_VALUE = 14;
+
+// 선택한 max life
+let chosenMaxLife = 100;
+// 현재 몬스터 체력
+let currentMonsterHealth = chosenMaxLife;
+// 현재 플레이어 체력
+let currentPlayerHealth = chosenMaxLife;
+
+
+adjustHealthBars(chosenMaxLife)
+
+function attackHandler() {
+    const damage = dealMonsterDamage(ATTAK_VALUE);
+    currentMonsterHealth -= damage;
+    
+    const playerDamage = dealPlayerDamage(MONSTER_ATTAK_VALUE);
+    currentPlayerHealth -= playerDamage;
+
+    if (currentMonsterHealth <= 0) {
+        alert('승리!');
+    } else if (currentPlayerHealth <= 0) {
+        alert('패배ㅠ');
+    }
+}
+
+attackBtn.addEventListener('click', attackHandler)
