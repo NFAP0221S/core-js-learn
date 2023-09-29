08f18eee91909ebed5646efce4e1404686269910
Hansol <102638663+NFAP0221S@users.noreply.github.com>
2023-09-29 22:47:36 +0900

    feat: 회복 기능 추가

--- 03-몬스터킬[제어문]/assets/scripts/app.js
+++ 03-몬스터킬[제어문]/assets/scripts/app.js
@@ -4,6 +4,8 @@ const ATTAK_VALUE = 10;
 const STRONG_ATTAK_VALUE = 17;
 // 몬스터 공격 값
 const MONSTER_ATTAK_VALUE = 14;
+// 체력 회복 값
+const HEAL_VAlUE = 20;
 
 // 선택한 max life
 let chosenMaxLife = 100;
@@ -12,20 +14,12 @@ let currentMonsterHealth = chosenMaxLife;
 // 현재 플레이어 체력
 let currentPlayerHealth = chosenMaxLife;
 
-
+// 유저 체력 바
 adjustHealthBars(chosenMaxLife)
 
-function attackMonster(mode) {
-    let maxDamage;
-    if (mode === 'ATTACK') {
-        maxDamage = ATTAK_VALUE;
-    } else if (mode === 'STRONG_ATTACK') {
-        maxDamage = STRONG_ATTAK_VALUE;
-    }
 
-    const damage = dealMonsterDamage(maxDamage);
-    currentMonsterHealth -= damage;
-    
+// 몬스터 공격 셋업
+function endRound() {
     const playerDamage = dealPlayerDamage(MONSTER_ATTAK_VALUE);
     currentPlayerHealth -= playerDamage;
 
@@ -38,13 +32,49 @@ function attackMonster(mode) {
     }
 }
 
+// 기본 공격 셋업
+function attackMonster(mode) {
+    let maxDamage;
+    if (mode === 'ATTACK') {
+        maxDamage = ATTAK_VALUE;
+    } else if (mode === 'STRONG_ATTACK') {
+        maxDamage = STRONG_ATTAK_VALUE;
+    }
+
+    const damage = dealMonsterDamage(maxDamage);
+    currentMonsterHealth -= damage;
+
+    // 공격 후 몬스터가 공격
+    endRound();
+}
+
+// 기본 공격
 function attackHandler() {
     attackMonster('ATTACK');
 }
-
+// 강한 공격
 function strongClickHandler() {
     attackMonster('STRONG_ATTACK');
 }
+// 회복
+function healPlayerHandler() {
+    let healValue;
 
-attackBtn.addEventListener('click', attackHandler)
-strongAttackBtn.addEventListener('click', strongClickHandler)
+    // 현재 체력이 (MaxLife - 힐) 보다 같거나 크면
+    if (currentPlayerHealth >= chosenMaxLife - HEAL_VAlUE) {
+        alert('체력을 회복할 수 없습니다.')
+        healValue = chosenMaxLife - currentPlayerHealth;
+    } else {
+        healValue = HEAL_VAlUE;
+    }
+    increasePlayerHealth(healValue);
+    currentPlayerHealth += healValue;
+
+    // 회복 후 몬스터가 공격
+    endRound();
+}
+
+
+attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongClickHandler);
+healBtn.addEventListener('click', healPlayerHandler);
